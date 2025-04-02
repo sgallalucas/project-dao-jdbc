39212f748f313b5897635eb5a62f5db2a61e52e5
Lucas Sgalla <lucasgalla10@gmail.com>
2025-04-02 15:51:35 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "Marketing");
+		System.out.println(dep);
+
+	}
+
+}
