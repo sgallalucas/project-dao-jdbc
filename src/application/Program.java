69b76644ba847f1a8cad2bfb08131597148db125
Lucas Sgalla <lucasgalla10@gmail.com>
2025-04-02 16:22:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dep = new Department(1, "Marketing");
-		System.out.println(dep);
+		
+		Seller sel = new Seller(5, "Fulano", "fulano@gmail.com", new Date(), 5000.0, dep);
+		
+		System.out.println(sel);
 
 	}
 
