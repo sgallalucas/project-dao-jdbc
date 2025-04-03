f2007399d6887448cb2744796c156c3ddd299067
Lucas Sgalla <lucasgalla10@gmail.com>
2025-04-03 14:13:07 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "Marketing");
-		
-		Seller sel = new Seller(5, "Fulano", "fulano@gmail.com", new Date(), 5000.0, dep);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller sel = sellerDao.findById(3);
 		
 		System.out.println(sel);
 
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 // Classe auxiliar responsável por instânciar os Daos
@@ -7,7 +8,7 @@ import model.dao.impl.SellerDaoJDBC;
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
